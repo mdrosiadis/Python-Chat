a9cdfb5f08e7a466df3140cefba56a0750bdb084
mdrosiadis <42861519+mdrosiadis@users.noreply.github.com>
2018-08-31 12:01:38 +0300

    Version 1.0

---
+++ client.py
@@ -0,0 +1,121 @@
+import socket
+import pyautogui
+import json
+from threading import Thread
+
+def checkData(data):
+
+    data = str(data,'utf-8')
+
+    if data[:6] != 'DACHAT':
+        #print('$- Denied request: ' , data)
+        return None
+
+    #print('$- Received: ', data)
+
+    data = data[6:]
+
+    data = json.loads(data)
+
+    return data
+
+def listenLoop(socket):
+
+	print('Now Listening for Incoming Messages!')
+
+	while True:
+
+		data = socket.recv(4096)
+
+		data = checkData(data)
+
+		if data['response'] == 'message' :
+			print(data['from'] + ': ' + data['message']) 
+
+		if data['response'] == 'quit' :
+			print('Quitting Chat!')
+			socket.close()
+			break
+
+
+
+
+
+target_host = "localhost"
+target_port = 9999
+
+username = pyautogui.prompt(text = 'Username:', title = 'Connect To Chat', default = '')
+password = pyautogui.password(text = 'Password:', title = 'Connect To Chat', default = '', mask = '*')
+
+data  = str(json.dumps({'username' : username, 'password' : password}))
+
+data = bytes('DACHAT'+data, 'utf-8')
+
+print(data)
+
+
+print(type(data))
+
+# create a socket object
+try:
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	client.connect((target_host,target_port))
+	client.send(data)
+	response = client.recv(4096)
+	
+	#response = str(response, 'utf-8')
+	response = checkData(response)
+
+	if response['response'] == 'connected':
+		myNickname = response['nickname']
+		print('Succesfully Connected as ' + myNickname + '!')
+		Thread(target = listenLoop, args = (client,)).start()
+		'''
+
+		if myNickname == 'Mike' :
+			toSend = 'DACHAT' + str(json.dumps({'type' : 'chat', 'target' : 'John', 'message' : 'KALHSPERA'}))
+			print(toSend)
+			client.send(bytes(toSend,'utf-8'))'''
+	
+	elif response['response'] == 'rejected' :
+		print(response['error'])
+		print('Client Closed conetion!')
+		client.close()
+		
+
+	print(response)
+
+	'''toSend = 'DACHAT' + str(json.dumps({'type' : 'send', 'target' : 'john', 'message' : 'KALHSPERA'}))
+
+	print(toSend)
+
+	client.send(bytes(toSend,'utf-8'))'''
+
+	print('Send messages with formtat: <nickname> -> <message> , or QUIT to quit')
+
+	while True:
+		userinput = input('$- ')
+
+		if userinput == 'QUIT':
+			client.send(bytes('DACHAT'+ str(json.dumps({'type' : 'quit'})),'utf-8'))
+			break
+
+		if ' -> ' in userinput:
+			data = userinput.split(' -> ')
+			if len(data) == 2 :
+				client.send(bytes('DACHAT' + str(json.dumps({'type' : 'chat', 'target' : data[0], 'message' : data[1]})),'utf-8'))
+			else :
+				print('WRONG FORMAT! TO MANY ->')
+		else :
+			print('WRONG FORMAT! NO ->')
+
+
+except Exception as e:
+	print(e)
+finally:
+	#client.close()
+	pass
+
+input()
+
+
---
+++ server.py
@@ -0,0 +1,135 @@
+﻿import socket
+import threading
+import json
+
+server_ip = 'localhost'
+server_port = 9999
+
+accounts = [{'username': 'mike', 'password' : 'mike', 'nickname' : 'Mike'}, 
+            {'username': 'john', 'password' : '1234', 'nickname' : 'John'}]
+
+
+activeClients = []
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((server_ip, server_port))
+
+server.listen(5)
+
+print('$- Listening on {}:{} '.format(server_ip, server_port))
+
+def handle_client(client_socket):
+
+    request = client_socket.recv(1024)
+
+    '''print('$- Received: ', request)
+
+    requeststring = str(request,'utf-8')
+
+    if requeststring[:6] != 'DACHAT':
+        print('$- Denied request: ' , requeststring)
+        client_socket.close()
+        return
+
+    requeststring = requeststring[6:]
+
+    data = json.loads(requeststring)'''
+
+    data = checkData(request)
+
+    if data == None:
+        client_socket.close()
+        return
+
+
+
+    found = False
+
+    for account in accounts:
+        if data['username'] == account['username']:
+            found = True
+            
+            if data['password'] == account['password']:
+                client_socket.send(bytes("DACHAT" + str(json.dumps({'response' : 'connected', 'nickname' : account['nickname']})),'utf-8'))
+                activeClients.append({'socket' : client_socket, 'nickname' : account['nickname']})
+                connectedClient(client_socket, account['nickname'])
+                #client_socket.send(bytes("Succesfully Connected as " + account['nickname'],'utf-8'))
+            else:
+                client_socket.send(bytes("DACHAT" + str(json.dumps({'response' : 'rejected', 'error' : 'Wrong Password'})),'utf-8'))
+                #client_socket.send(bytes("Wrong Password!",'utf-8')
+                break
+    if not found:
+        #client_socket.send(bytes("Unknown Username!",'utf-8'))
+        client_socket.send(bytes("DACHAT" + str(json.dumps({'response' : 'rejected', 'error' : 'Username not Found!'})),'utf-8'))
+
+    #client_socket.send(bytes("1K για το chain 300 για το φουτερ",'utf-8'))
+
+    #client_socket.close()
+
+def connectedClient(client_socket, nickname):
+
+    print(nickname + ' connected!')
+
+    while True:
+
+        data = client_socket.recv(4096)
+
+        data = checkData(data)
+
+        print('Got DATA')
+
+        if data == None:
+            print('Server Closed conetion!')
+            client_socket.close()
+            return
+
+        if data['type'] == 'quit' :
+            client_socket.send(bytes('DACHAT' + str(json.dumps({'response' : 'quit'})), 'utf-8'))
+            print(nickname + ' disconected!')
+            for client in activeClients:
+                if nickname == client['nickname']:
+                    activeClients.remove(client)
+                    break
+            client_socket.close()
+            return
+        if data['type'] == 'chat' :
+            userFound = False
+            for client in activeClients:
+                if data['target'] == client['nickname']:
+                    print(nickname + ' sending a message to ' + client['nickname'] +'!')
+                    client['socket'].send(bytes('DACHAT' + str(json.dumps({'response' : 'message', 'from' : nickname, 'message' : data['message']})), 'utf-8'))
+                    userFound = True
+                    break
+            if not userFound :
+                client_socket.send(bytes('DACHAT'+str(json.dumps({'response' : 'message', 'from' : 'SYSTEM', 'message' : 'ERROR: User ' + data['target'] + ' not found!'})),'utf-8'))
+
+
+#data in bytes, return dict if the received data were accepted, else None
+def checkData(data):
+
+    data = str(data,'utf-8')
+
+    if data[:6] != 'DACHAT':
+        print('$- Denied request: ' , data)
+        return None
+
+    print('$- Received: ', data)
+
+    data = data[6:]
+
+    data = json.loads(data)
+
+    return data
+
+
+
+
+while True:
+
+    client, addr = server.accept()
+
+    print('Accepted connection from: {}:{}'.format(addr[0], addr[1]))
+
+    client_handler = threading.Thread(target=handle_client, args=(client,))
+    client_handler.start()
